b88b08f6f5893fef58ee9085967a723b540a2ba0
cereceda1991 <cereceda1991@gmail.com>
2023-04-02 19:02:34 -0500

    Update

--- database/config.js
+++ database/config.js
@@ -1,16 +1,12 @@
 const { Sequelize } = require('sequelize');
-const pg = require('pg');
 
-const db = new Sequelize(
-  process.env.DB_DATABASE,
-  process.env.DB_USERNAME,
-  process.env.DB_PASSWORD,
-  {
-    dialect: process.env.DB_DIALECT,
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    logging: false,
-  }
-);
+const db = new Sequelize({
+  dialect: process.env.DB_DIALECT,
+  host: process.env.DB_HOST,
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
+  logging: false,
+});
 
 module.exports = { db };
